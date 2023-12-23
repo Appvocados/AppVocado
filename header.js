bb3c6b8b662944d46ad6c4050a44cbacd9ec7d88
Appvocados <145595569+Appvocados@users.noreply.github.com>
2023-12-23 13:48:18 -0600

    Update header.js

--- header.js
+++ header.js
@@ -1,4 +1,3 @@
-// header.js
 function mostrarHeader() {
     document.write(`
         <header>
@@ -6,54 +5,77 @@ function mostrarHeader() {
                 <div id="appvocado">
                     <figure class="icon">
                         <a href="index.html">
-                        <img src="avocado-1-icon.gif" title="Click para ir a página de inicio" alt="Inicio">
+                            <img src="img/avocado-1-icon.gif" title="Click para ir a página de inicio" alt="Inicio">
                         </a>
                     </figure>
                     <a id="AppVocado" href="index.html" class="titles"> AppVocado </a>              
                 </div>
-                
-                <!-- 
-                <form class="buscador">
-                    <input id="src" type="text" placeholder="Buscar fecha o noticia">
-                    <input id="reset" type="reset" value="X">   	
-                    <input id="buscar" type="submit" value="Buscar">
-                </form>
-                 -->
 
-                <!--
-                <a id="" class="link" href="Sell Form.html">Entrar</a>
-                -->
-    
                 <a id="AppVocado" href="index.html" class="titles">
-                <i class="fa-solid fa-chart-line fa-sm" style="color: #80ff00;"></i></a>
+                    <i class="fa-solid fa-chart-line fa-sm" style="color: #80ff00;"></i>
+                </a>
 
-                <!-- Menú desplegable con el ícono FontAwesome -->
-<div class="dropdown">
-    <button class="dropbtn">
-        <i class="fa-solid fa-bars fa-2xl" style="color: #80ff00;"></i>
-    </button>
-    <div class="dropdown-content right-aligned full-width">
-        <a id="" class="link alert" href="#"><i class="fa-regular fa-user iconosBlack"></i>Ingresar</a>
-        <a id="" class="link" href="indexRegistroDeHuertos.html"><i class="fa-solid fa-file iconosBlack"></i></i>Guía para veder</a>
-        <a id="" class="link alert" href="mihuerto.html"><i class="fa-solid fa-seedling iconosBlack"></i>Mi huerto</a>
-        <a id="" class="link alert" href="#"><i class="fa-solid fa-bug-slash iconosBlack"></i>Consejos para cultivos</a>
-        <a id="" class="link alert" href="#"><i class="fa-solid fa-user-xmark iconosBlack"></i>Salir</a>
-
-
-
-        </a>
-
-        <!-- 
-        <a href="showData.html">clientes</a>
-        <a href="FormDataGrafic.html">Actualizar precios</a>
-        <a href="visualizargraficos.html">Ver gráficas</a>
-        -->
-    </div>
-</div>
-
-         </div>
-
+                <div class="dropdown">
+                <button class="dropbtn" onmouseover="showMenu()">
+                        <i class="fa-solid fa-bars fa-2xl" style="color: #80ff00;"></i>
+                    </button>
+                    <div class="dropdown-content right-aligned full-width">
+                        <a id="" class="link alert" href="#"><i class="fa-regular fa-user iconosBlack"></i>Ingresar</a>
+                        <a id="" class="link" href="indexRegistroDeHuertos.html"><i class="fa-solid fa-file iconosBlack"></i></i>Guía para vender</a>
+                        <a id="" class="link alert" href="#"><i class="fa-solid fa-newspaper iconosBlack"></i>Noticias</a>
+                        <a id="" class="link alert" href="mihuerto.html"><i class="fa-solid fa-seedling iconosBlack"></i>Mi huerto</a>
+                        <a id="" class="link alert" href="#"><i class="fa-solid fa-bug-slash iconosBlack"></i>Consejos para cultivos</a>
+                        <a id="" class="link alert" href="#"><i class="fa-solid fa-user-xmark iconosBlack"></i>Salir</a>
+                        
+                        <a id="cerrar" class="hoverRed" href="#" onclick="hideMenu()"><i class="fa-solid fa-square-xmark iconosBlack "></i>Salir del menú</a>
+                    </div>
+                </div>
             </nav>
         </header>
+    `);
+
+        // Función para ocultar el menú al hacer clic en "Salir"
+        document.write(`
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                var salirLink = document.getElementById('salir');
+                if (salirLink) {
+                    salirLink.addEventListener('click', function() {
+                        hideMenu();
+                    });
+                }
+            });
+
+            // Función para ocultar el menú
+            function hideMenu() {
+                var menu = document.querySelector('.dropdown-content');
+                if (menu) {
+                    menu.style.display = 'none';
+                }
+            }
+
+            // Función para mostrar el menú al pasar el cursor sobre el botón
+            function showMenu() {
+                var menu = document.querySelector('.dropdown-content');
+                if (menu) {
+                    menu.style.display = 'block';
+                }
+            }
+
+            // funcion de alertas para funciones que aun no estan
+            // Obtener todos los elementos con la clase "alert"
+            var elementosLinks = document.getElementsByClassName("alert");
+            
+            // Agregar un evento de clic a cada elemento con la clase "alert"
+            for (var i = 0; i < elementosLinks.length; i++) {
+                elementosLinks[i].addEventListener("click", function(event) {
+                    event.preventDefault();
+                    // Mostrar una alerta
+                    alert("Estamos trabajando para mejorar el sitio, esta función no está disponible aún. Gracias por tu paciencia. \u{1F622} \u{1F6E0}");
+                });
+            }
+            
+
+        </script>
     `);
 }
